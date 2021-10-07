446a6f4650eaed664b04fb410714e4b853492747
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-10-07 09:18:43 +0200

    36. Implementing custom jdbc rowmapper

--- src/main/java/com/kash/jdbc/PersonJdbcDao.java
+++ src/main/java/com/kash/jdbc/PersonJdbcDao.java
@@ -4,8 +4,11 @@ import com.kash.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,16 +18,27 @@ public class PersonJdbcDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    class PersonRowMapper implements RowMapper<Person>{
+
+        @Override
+        public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Person person = new Person();
+            person.setId(rs.getInt("id"));
+            person.setName(rs.getString("name"));
+            person.setLocation(rs.getString("location"));
+            person.setBirthDate(rs.getDate("birth_date"));
+            return person;
+        }
+    }
     //
     public List<Person> findAll(){
       return jdbcTemplate.query("select * from person",
-              new BeanPropertyRowMapper(Person.class));
+              new PersonRowMapper());
     }
 
     public Person findById(int id){
         return jdbcTemplate.queryForObject
-                ("select * from person where id=?", new Object[]{id},
-                new BeanPropertyRowMapper<Person>(Person.class));
+                ("select * from person where id=?", new PersonRowMapper());
     }
 
     public int deleteById(int id){
